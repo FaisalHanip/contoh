5f6ccd141a76e9178a69a11205c1b5cd1ecac469
FaisalHanip <90999101+FaisalHanip@users.noreply.github.com>
2025-12-19 13:23:03 +0700

    Add script.js for guest greeting and countdown

---
+++ script.js
@@ -0,0 +1,39 @@
+/* WA LINK */
+const urlParams = new URLSearchParams(window.location.search);
+const namaTamu = urlParams.get('to');
+if (namaTamu) {
+  document.getElementById("tamu").innerText =
+    "Kepada Yth: " + namaTamu;
+}
+
+/* MUSIC */
+const music = document.getElementById("bgMusic");
+music.play();
+// // let isPlay = true;
+// // function toggleMusic() {
+// //   isPlay ? music.pause() : music.play();
+// //   isPlay = !isPlay;
+// }
+
+/* COUNTDOWN */
+const targetDate = new Date("2026-01-20T09:00:00").getTime();
+setInterval(() => {
+  const now = new Date().getTime();
+  const diff = targetDate - now;
+
+  document.getElementById("days").innerText = Math.floor(diff / (1000*60*60*24));
+  document.getElementById("hours").innerText = Math.floor(diff / (1000*60*60)%24);
+  document.getElementById("minutes").innerText = Math.floor(diff / (1000*60)%60);
+  document.getElementById("seconds").innerText = Math.floor(diff / 1000%60);
+}, 1000);
+
+/* COPY REKENING */
+function copyRekening() {
+  navigator.clipboard.writeText("1234567890");
+  document.getElementById("notif").innerText = "Rekening disalin ✔";
+}
+
+/* SCROLL */
+function scrollToSection(id) {
+  document.querySelector("." + id)?.scrollIntoView({ behavior: "smooth" });
+}
